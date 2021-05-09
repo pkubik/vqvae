1371f66b1f096ee5cf21ecd83a2a286898508fcc
Pawel Kubik <pawel.kubik@tcl.com>
2021-05-09 02:49:24 +0200

    Improve VQVAE learning params

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ Hyperparameters
 timestamp = utils.readable_timestamp()
 
 parser.add_argument("--batch_size", type=int, default=32)
-parser.add_argument("--n_updates", type=int, default=5000)
+parser.add_argument("--n_updates", type=int, default=100000)
 parser.add_argument("--n_hiddens", type=int, default=128)
 parser.add_argument("--n_residual_hiddens", type=int, default=32)
-parser.add_argument("--n_residual_layers", type=int, default=2)
+parser.add_argument("--n_residual_layers", type=int, default=4)
 parser.add_argument("--embedding_dim", type=int, default=64)
-parser.add_argument("--n_embeddings", type=int, default=512)
+parser.add_argument("--n_embeddings", type=int, default=256)
 parser.add_argument("--beta", type=float, default=.25)
 parser.add_argument("--learning_rate", type=float, default=3e-4)
 parser.add_argument("--log_interval", type=int, default=50)
-parser.add_argument("--dataset",  type=str, default='CIFAR10')
+parser.add_argument("--dataset", type=str, default='CIFAR10')
 
 # whether or not to save model
 parser.add_argument("--filename",  type=str, default='model')
